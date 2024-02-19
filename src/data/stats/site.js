112f6fa8555c5626867a6ef61019dd0908576ca8
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2024-02-20 02:04:10 +0600

    Update site.js

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -1,28 +1,28 @@
 /* Keys match keys returned by the github api. Fields without keys are
  * mostly jokes. To see everything returned by the github api, run:
- curl https://api.github.com/repos/arefinx/personal-site
+ curl https://api.github.com/repos/mldangelo/personal-site
  */
 const data = [
   {
     label: 'The date this site was created',
-    value: 'Tuesday - February 20 - 2024',
+    value: 'Wednessday - April 19 - 2023',
   },
   {
-    label: 'Placeholder',
-    value: '',
+    label: 'The purpose of this webpage',
+    value: 'This was actually done as a fun project',
   },
   {
-    label: 'Placeholder',
-    value: '0',
+    label: 'This webpage is based on',
+    value: 'JavaScript and SCSS and a little bit of HTML',
   },
   {
-    label: 'Placeholder',
-    value: '0',
+    label: 'This webpage was updated on',
+    value: 'Friday - December 1 - 2023',
   },
   {
-    label: 'Placeholder',
-    key: 'placeholder',
-    link: 'https://arefinn.vercel.app',
+    label: 'All rights reserved by',
+    value: '2023 © Capsule Corporation.',
+  },
 ];
 
 export default data;
